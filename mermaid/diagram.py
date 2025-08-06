18c25c4189efa7d1502d68a8def39bc750f0efb8
Matthew Hamilton <mwhamilton6@gmail.com>
2025-08-06 15:37:14 -0500

    initial commit

---
+++ mermaid/diagram.py
@@ -0,0 +1,32 @@
+from enum import StrEnum
+from typing import Sequence
+from .node import Node
+from .link import Link
+
+
+class Orientation(StrEnum):
+    default = ""
+    top_to_bottom = "TB"
+    top_down = "TD"
+    bottom_to_top = "BT"
+    right_to_left = "RL"
+    left_to_right = "LR"
+
+class MermaidDiagram:
+    title: str = ""
+    nodes: list[Node]
+    links: list[Link]
+    orientation: Orientation
+
+    def to_markdown(self) -> str:
+        node_text = "\n".join(
+            n.to_markdown()
+            for n in self.nodes
+        )
+        link_text = "\n".join(
+            l.to_markdown()
+            for l in self.links
+        )
+        header = f"---\ntitle: {self.title}\n---" if self.title else ""
+        graph_defines = f"graph {self.orientation}"
+        return f"{header}\n{graph_defines}\n{node_text}\n{link_text}"
