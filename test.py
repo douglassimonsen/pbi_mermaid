70bb261c66d2f4e84c941d318b7e3b5be79ab111
Matthew Hamilton <mwhamilton6@gmail.com>
2025-08-06 15:40:32 -0500

    Deleting local files

--- test.py
+++
@@ -1,7 +0,0 @@
-from mermaid import Link, MermaidDiagram, Node
-
-n1 = Node("hi", style="fill:#f9f,stroke:#333,stroke-width:4px")
-n2 = Node("bye")
-link = Link(n1, n2)
-diagram = MermaidDiagram([n1, n2], [link])
-print(diagram.show())
